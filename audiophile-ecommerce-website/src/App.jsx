43adf1873bcd3a03367425600201688f432e37e7
kaikaci12 <somewonesomewone@gmail.com>
2024-05-03 18:02:07 +0400

    initial setup

---
+++ audiophile-ecommerce-website/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import HomeComponent from "./components/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <HomeComponent />;
+    </div>
+  );
+}
+
+export default App;
