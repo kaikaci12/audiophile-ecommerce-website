acb9f2ba2a8acc2f04960a52dc65e361c297ca64
kaikaci12 <somewonesomewone@gmail.com>
2024-05-05 00:49:06 +0400

    fix ZX7 product styles

--- audiophile-ecommerce-website/src/components/ZX7Speaker.jsx
+++ audiophile-ecommerce-website/src/components/ZX7Speaker.jsx
@@ -1,20 +1,25 @@
 import ZX7 from "../assets/ZX7.png";
 import Button from "./Button";
+
 function ZX7Speaker() {
   return (
-    <div className="w-full h-[320px] rounded-[8px] bg-[#F1F1F1] flex flex-col justify-center items-center ">
-      <div>
+    <div
+      className="w-full h-[320px] rounded-[8px] bg-[#F1F1F1] bg-cover bg-center flex flex-col justify-center items-center"
+      style={{ backgroundImage: `url(${ZX7})` }}
+    >
+      <div className="z-[3]">
         <h2 className="text-[#000] text-[28px] font-bold tracking-[2px]">
           ZX7 SPEAKER
         </h2>
+
         <Button
           className={
             "border-[1px] border-[solid] border-[#000] bg-transparent text-[black]"
           }
         />
-        <img src={ZX7} alt="" className="absolute " />
       </div>
     </div>
   );
 }
+
 export default ZX7Speaker;
